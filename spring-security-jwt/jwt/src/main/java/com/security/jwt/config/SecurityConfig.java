5fe83263d223a0a0ef9cb5b853f1f17c0f21c694
SeungCheol <ssc6839@gmail.com>
2021-09-24 20:22:00 +0900

    SecurityConfig 주석 처리

--- spring-security-jwt/jwt/src/main/java/com/security/jwt/config/SecurityConfig.java
+++ spring-security-jwt/jwt/src/main/java/com/security/jwt/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http  //HttpSecurity 객체 설정
                 .csrf().disable()
                 //세션 사용하지 않겠다는 의미
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -27,9 +27,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().disable()
                 .httpBasic().disable()
                 .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                //권한요청 처리 설정 메서드임
                 .authorizeRequests()
                 .antMatchers("/api/v1/user/**").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+                //"/api/v1/admin/**" 주소는 ROLE_ADMIN 권한만 접근 가능
                 .antMatchers("/api/v1/admin/**").access("hasRole('ROLE_ADMIN')")
+                //다른 요청은 누구든지 접근할 수 있음
                 .anyRequest().permitAll();
     }
 }
